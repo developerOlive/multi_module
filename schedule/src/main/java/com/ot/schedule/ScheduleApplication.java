02fde4316d080e5fd5ec567e7a2802a2b8dab640
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:31:33 +0900

    Update ScheduleApplication.java

--- schedule/src/main/java/com/ot/schedule/ScheduleApplication.java
+++ schedule/src/main/java/com/ot/schedule/ScheduleApplication.java
@@ -1,11 +1,14 @@
 package com.ot.schedule;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableBatchProcessing
 @SpringBootApplication
+@EnableScheduling
 public class ScheduleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
     }
