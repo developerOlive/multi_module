c9d74a406d6ef8e16ac42dc8ac5772b14d6a7433
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:26:43 +0900

    Update AdminApplication.java

--- admin/src/main/java/com/ot/admin/AdminApplication.java
+++ admin/src/main/java/com/ot/admin/AdminApplication.java
@@ -1,13 +1,19 @@
 package com.ot.admin;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ot.*"})
+@MapperScan(basePackages = {"com.ot.service.mapper"})
+@SecurityScheme(name = "jwtapi", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class AdminApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
 
 }
