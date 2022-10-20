b73c497b9875144daa237f3c7e768825b41441df
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:17:47 +0900

    Update JwtFilter.java

--- service/src/main/java/com/ot/service/jwt/JwtFilter.java
+++ service/src/main/java/com/ot/service/jwt/JwtFilter.java
@@ -1,4 +1,70 @@
 package com.ot.service.jwt;
 
-public class JwtFilter {
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+    Request Header에서 Authorization값을 꺼내어 토큰을 검사하고,
+    유저 정보를 꺼내서 SecurityContext에 저장하는 역할을 합니다.
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+    private final CustomUserDetailService userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String token = null;
+        String userName = null;
+
+        // authorizationHeader에 값이 있고, 이 값이 "Bearer "로 시작한다면 authorizationHeader를 통해 token을 추출
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7); // "Bearer "가 7자
+            try {
+                userName = tokenProvider.extractUsername(token);
+            } catch (IllegalArgumentException e) {
+                log.info("Unable to get JWT Token");
+            } catch (ExpiredJwtException e) {
+                log.info("JWT Token has expired");
+            }
+        } else {
+            log.info("JWT Token does not begin with Bearer String");
+        }
+
+        // 토큰 유효성 검사 - userName이 null이 아니면 token값이 정상
+        // SecurityContextHolder의 authentication이 비어 있다면 최초 인증
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailService.loadUserByUsername(userName);
+
+            // 토큰이 유효한 경우, 인증용 객체 (UsernamePasswordAuthenticationToken) 생성
+            if (tokenProvider.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // 인증된 자세한 정보를 SecurityContextHolder에 세팅
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
