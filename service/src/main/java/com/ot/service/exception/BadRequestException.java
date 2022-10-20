05be36c47fe789c46b1a13b1b26fa94abe4e25b2
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:15:38 +0900

    Update BadRequestException.java

--- service/src/main/java/com/ot/service/exception/BadRequestException.java
+++ service/src/main/java/com/ot/service/exception/BadRequestException.java
@@ -1,4 +1,47 @@
 package com.ot.service.exception;
 
-public class BadRequestException {
+import com.ot.service.config.ErrorCodeEnum;
+import lombok.Getter;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+public class BadRequestException extends RuntimeException {
+
+    private static final long serialVersionUID = -4688025679056011205L;
+
+    private static ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+    static {
+        messageSource.setBasenames("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+    }
+
+    @Getter
+    private String errorCode;
+    private String errorMessage = "";
+
+    @Override
+    public String getMessage() {
+        return this.getMessage(new Locale("en"));
+    }
+
+    public String getMessage(Locale locale) {
+        return this.errorMessage;
+    }
+
+    public BadRequestException(){
+    }
+
+    public BadRequestException(String errorCode) {
+        super();
+        this.errorCode = errorCode;
+    }
+
+    public BadRequestException(ErrorCodeEnum e) {
+        super();
+        this.errorCode = e.getCode();
+        this.errorMessage = messageSource.getMessage(e.getMessage(), null, "BadRequestException", new Locale("en"));
+    }
 }
