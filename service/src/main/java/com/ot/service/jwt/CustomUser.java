14c99f95efcb2e4712973c069f38a8127d050a06
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:17:14 +0900

    Update CustomUser.java

--- service/src/main/java/com/ot/service/jwt/CustomUser.java
+++ service/src/main/java/com/ot/service/jwt/CustomUser.java
@@ -1,4 +1,23 @@
 package com.ot.service.jwt;
 
-public class CustomUser {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+public class CustomUser extends User {
+    private Long id;
+
+    public CustomUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public CustomUser(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.id = id;
+    }
 }
