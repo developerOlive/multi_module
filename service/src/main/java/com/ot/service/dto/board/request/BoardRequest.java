86c6ffa44642730875424d91763065397f2308ca
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:12:55 +0900

    Update BoardRequest.java

--- service/src/main/java/com/ot/service/dto/board/request/BoardRequest.java
+++ service/src/main/java/com/ot/service/dto/board/request/BoardRequest.java
@@ -1,4 +1,23 @@
 package com.ot.service.dto.board.request;
 
-public class BoardRequest {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "게시글수정데이터", name = "PutBoard")
+public class PutBoard {
+
+    @Schema(description = "인덱스", example = "")
+    private Long id;
+
+    @Schema(description = "제목", example = "Title 2")
+    private String title;
+
+    @Schema(description = "내용", example = "Contents 2")
+    private String contents;
+
+    @Schema(description = "수정자", example = "2")
+    private Long updUserSeq;
 }
