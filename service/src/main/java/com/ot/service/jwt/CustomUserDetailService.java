0e07c18d0b9e6a569447d58d48ce9e36a4233632
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:17:32 +0900

    Update CustomUserDetailService.java

--- service/src/main/java/com/ot/service/jwt/CustomUserDetailService.java
+++ service/src/main/java/com/ot/service/jwt/CustomUserDetailService.java
@@ -1,4 +1,33 @@
 package com.ot.service.jwt;
 
-public class CustomUserDetailService {
+import com.ot.service.dto.user.response.UserDto;
+import com.ot.service.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+    DB에서 사용자 정보를 가져오는 역할을 합니다.
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+
+        UserDto userDto = userMapper.getUserByUserId(userId);
+        List<String> roles = List.of(userDto.getRoles().split(","));
+        List<GrantedAuthority> grantedAuthorities = roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return new CustomUser(userDto.getId(), userDto.getUserId(), userDto.getPassword(), grantedAuthorities);
+        //return new CustomUser(200L,"userId@naver.com", "password", null);
+    }
 }
