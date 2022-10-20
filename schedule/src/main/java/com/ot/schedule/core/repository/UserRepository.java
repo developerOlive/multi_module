35e0c779bb764c0c06966449172f6bfc19526e62
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:30:04 +0900

    Update UserRepository.java

--- schedule/src/main/java/com/ot/schedule/core/repository/UserRepository.java
+++ schedule/src/main/java/com/ot/schedule/core/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.ot.schedule.core.repository;
 
-public interface UserRepository {
+import com.ot.schedule.core.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByLastLoginDateBeforeAndStatusEquals(LocalDateTime lastLoginDate, User.Status status);
+    List<User> findAllByUpdateDateBetweenAndStatus(LocalDateTime before, LocalDateTime now, User.Status status);
 }
