0f5c2d803935a453acf77a9ea67fc96af30c0e5b
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:18:57 +0900

    Update LogService.java

--- service/src/main/java/com/ot/service/service/LogService.java
+++ service/src/main/java/com/ot/service/service/LogService.java
@@ -1,4 +1,29 @@
 package com.ot.service.service;
 
+import com.ot.service.dto.log.ApiLogHistoryDto;
+import com.ot.service.mapper.LogMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 로그에 대한 비즈니스 로직을 담당합니다.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class LogService {
+
+    private final LogMapper logMapper;
+
+    @Async
+    public void insertLog(ApiLogHistoryDto param) {
+
+        log.info("[Insert Log Service Call]");
+
+        logMapper.insertLog(param);
+    }
 }
