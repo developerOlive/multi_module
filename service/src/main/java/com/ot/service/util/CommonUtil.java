a1b303be9970393d12f361b0ccb5f4b36f1a9f21
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:19:22 +0900

    Update CommonUtil.java

--- service/src/main/java/com/ot/service/util/CommonUtil.java
+++ service/src/main/java/com/ot/service/util/CommonUtil.java
@@ -1,4 +1,34 @@
 package com.ot.service.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class CommonUtil {
+
+    public static String getClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+        }
+
+        if(ip != null && StringUtils.isNotEmpty(ip) && ip.indexOf(",") > -1) {
+            ip = ip.substring(0, ip.indexOf(","));
+        }
+
+        return ip;
+    }
 }
