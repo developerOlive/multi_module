e3986a7ca53f5577a55101bd2e533815a8ad2c8b
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:14:00 +0900

    Update ApiLogHistoryDto.java

--- service/src/main/java/com/ot/service/dto/log/ApiLogHistoryDto.java
+++ service/src/main/java/com/ot/service/dto/log/ApiLogHistoryDto.java
@@ -1,4 +1,21 @@
 package com.ot.service.dto.log;
 
-public class ApiLogHistoryDto {
+@Getter
+@Setter
+@Builder
+public class ApiLogHistoryDto implements Serializable {
+
+    private Long id;
+    private String logTypeCd;
+    private String path;
+    private String reqMsg;
+    private String resMsg;
+    private Double durationTime;
+    private String isError;
+    private String errorCd;
+    private String errorMsg;
+    private LocalDateTime startDt;
+    private LocalDateTime endDt;
+    private String clientIpAddr;
+    private Long threadId;
 }
