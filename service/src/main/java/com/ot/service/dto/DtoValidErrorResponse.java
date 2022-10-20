44f14af5db0a19f22adff64257e6f1c71afc810a
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:15:13 +0900

    Update DtoValidErrorResponse.java

--- service/src/main/java/com/ot/service/dto/DtoValidErrorResponse.java
+++ service/src/main/java/com/ot/service/dto/DtoValidErrorResponse.java
@@ -1,4 +1,17 @@
 package com.ot.service.dto;
 
-public class DtoValidErrorResponse {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+@Schema(description = "DTO 유효성 에러 응답 결과")
+@Builder
+public class DtoValidErrorResponse implements Serializable {
+
+    private String param;
+    private String errorMessage;
 }
