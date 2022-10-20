f22519aa6cf26824b891830fd120814be192c3bb
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:18:23 +0900

    Update LogMapper.java

--- service/src/main/java/com/ot/service/mapper/LogMapper.java
+++ service/src/main/java/com/ot/service/mapper/LogMapper.java
@@ -1,4 +1,9 @@
 package com.ot.service.mapper;
 
+import com.ot.service.dto.log.ApiLogHistoryDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface LogMapper {
+    void insertLog(ApiLogHistoryDto param);
 }
