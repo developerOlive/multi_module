95f5ab89545e00735acbedbba787af701740f228
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:18:33 +0900

    Update UserMapper.java

--- service/src/main/java/com/ot/service/mapper/UserMapper.java
+++ service/src/main/java/com/ot/service/mapper/UserMapper.java
@@ -1,4 +1,22 @@
 package com.ot.service.mapper;
 
+import com.ot.service.dto.user.request.UserPostDto;
+import com.ot.service.dto.user.request.UserPutDto;
+import com.ot.service.dto.user.request.UserRoleUpdateDto;
+import com.ot.service.dto.user.response.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface UserMapper {
+    List<UserDto> selectUserAll();
+    UserDto selectUserById(@Param("id") Long id);
+    void insertUser(UserPostDto param);
+    UserDto getUserByUserId(String userId);
+    void updateUser(UserPutDto param);
+    void deleteUserById(Map<String, Long> map);
+    void updateUserRole(UserRoleUpdateDto param);
 }
