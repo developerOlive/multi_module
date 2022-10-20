f94a074a8ae6db469a02143665d15ed4080f194b
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:12:12 +0900

    Update TokenDto.java

--- service/src/main/java/com/ot/service/dto/Login/TokenDto.java
+++ service/src/main/java/com/ot/service/dto/Login/TokenDto.java
@@ -1,4 +1,19 @@
 package com.ot.service.dto.Login;
 
-public class TokenDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Schema(description = "토큰 Dto")
+@Builder
+public class TokenDto implements Serializable {
+
+    @Schema(required = true, description = "인증토큰")
+    private String accessToken;
+
+    @Schema(required = true, description = "재발행토큰")
+    private String refreshToken;
 }
