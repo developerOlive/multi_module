c443dab73208a09355b307b2dba794693789c882
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:18:12 +0900

    Update BoardMappr.java

--- service/src/main/java/com/ot/service/mapper/BoardMappr.java
+++ service/src/main/java/com/ot/service/mapper/BoardMappr.java
@@ -1,4 +1,22 @@
 package com.ot.service.mapper;
 
-public interface BoardMappr {
+import com.ot.service.dto.board.request.PostBoard;
+import com.ot.service.dto.board.request.PutBoard;
+import com.ot.service.dto.board.response.Board;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    void insertBoard(PostBoard param);
+
+    void updateBoard(PutBoard param);
+
+    Board selectBoard(@Param("id") Long id);
+
+    List<Board> selectBoardAll();
+
+    void deleteBoard(@Param("id") Long id);
 }
