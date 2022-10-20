005dacbfb0b15048ee0038a8d7c578ded8fd431f
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:11:51 +0900

    Update LoginDto.java

--- service/src/main/java/com/ot/service/dto/Login/LoginDto.java
+++ service/src/main/java/com/ot/service/dto/Login/LoginDto.java
@@ -1,4 +1,17 @@
 package com.ot.service.dto.Login;
 
-public class LoginDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Schema(description = "로그인 Dto")
+public class LoginDto implements Serializable {
+
+    @Schema(required = true, description = "사용자 아이디", example = "userId@overtake.kr")
+    private String username;
+
+    @Schema(required = true, description = "비밀번호", example = "password")
+    private String password;
 }
