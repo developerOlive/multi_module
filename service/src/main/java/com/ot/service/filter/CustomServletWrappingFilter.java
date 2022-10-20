02cb713c0bb7b4352a0c8a80b397bc4849ae411e
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-21 07:16:30 +0900

    Update CustomServletWrappingFilter.java

--- service/src/main/java/com/ot/service/filter/CustomServletWrappingFilter.java
+++ service/src/main/java/com/ot/service/filter/CustomServletWrappingFilter.java
@@ -1,4 +1,29 @@
 package com.ot.service.filter;
 
-public class CustomServletWrappingFilter {
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomServletWrappingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        /**
+         * 인터셉터에서 로그를 찍기 전 ContentCachingWrapper 로 래핑 처리
+         *
+         * response 의 경우 copyBodyToResponse(); 로 body 값을 copy 해서 캐시로 저장해두었다가 Interceptor 에서 다시 읽는다.
+         * */
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        responseWrapper.copyBodyToResponse();
+    }
 }
