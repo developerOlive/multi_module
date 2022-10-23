daada0bb267fa4c5f46c264c94e921851c84cd27
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-24 07:22:49 +0900

    Update DuplicateValidationProcessor.java

--- schedule/src/main/java/com/ot/schedule/processor/DuplicateValidationProcessor.java
+++ schedule/src/main/java/com/ot/schedule/processor/DuplicateValidationProcessor.java
@@ -1,4 +1,37 @@
 package com.ot.schedule.processor;
 
-public class DuplicateValidationProcessor {
+import org.springframework.batch.item.ItemProcessor;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+public class DuplicateValidationProcessor<T> implements ItemProcessor<T, T> {
+
+    private final Map<String, Object> keyPool = new ConcurrentHashMap<>();
+    private final Function<T, String> keyExtractor;
+    private final boolean allowDuplicate;
+
+    public DuplicateValidationProcessor(Function<T, String> keyExtractor,
+                                        boolean allowDuplicate) {
+
+        this.keyExtractor = keyExtractor;
+        this.allowDuplicate = allowDuplicate;
+    }
+
+    @Override
+    public T process(T item) throws Exception {
+        if (allowDuplicate) {
+            return item; // 필터링 진행하지 않음
+        }
+
+        String key = keyExtractor.apply(item);
+
+        if (keyPool.containsKey(key)) {
+            return null;
+        }
+
+        keyPool.put(key, key);
+        return item;
+    }
 }
