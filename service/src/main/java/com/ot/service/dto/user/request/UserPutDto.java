5e0bf5babc6086d273699793f7d746099a917bea
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-24 07:19:36 +0900

    Update UserPutDto.java

--- service/src/main/java/com/ot/service/dto/user/request/UserPutDto.java
+++ service/src/main/java/com/ot/service/dto/user/request/UserPutDto.java
@@ -1,4 +1,34 @@
 package com.ot.service.dto.user.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Schema(description = "사용자 정보 수정", name = "UserPutDto")
 public class UserPutDto {
+
+    @Schema(description = "seq", example = "1")
+    private Long id;
+
+    @Schema(description = "패스워드", example = "password")
+    private String password;
+
+    @Schema(description = "이름", example = "이름")
+    private String name;
+
+    @Schema(description = "휴대폰번호", example = "01012345678")
+    private String phone;
+
+    @JsonIgnore
+    @Schema(description = "수정자", example = "ID")
+    private Long updUserSeq;
+
+    @JsonIgnore
+    @Schema(description = "수정일시", example = "2022-09-01 12:12:12")
+    private LocalDateTime updateDate;
 }
