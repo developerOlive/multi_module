e670d10b5c19f743d8715c6e2e77e6c97a81911b
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-24 07:22:37 +0900

    Update SavePersonListener.java

--- schedule/src/main/java/com/ot/schedule/listner/SavePersonListener.java
+++ schedule/src/main/java/com/ot/schedule/listner/SavePersonListener.java
@@ -1,4 +1,71 @@
-package com.ot.schedule.listner;
+package com.ot.schedule.listener;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeJob;
+import org.springframework.batch.core.annotation.BeforeStep;
+
+@Slf4j
 public class SavePersonListener {
+
+    // interface로 JobExecutionListener 생성
+    public static class SavePersonJobExecutionListener implements JobExecutionListener {
+
+        // Job 실행 전 호출
+        @Override
+        public void beforeJob(JobExecution jobExecution) {
+            log.info("beforeJob");
+        }
+
+        // Job 실행 후 호출
+        @Override
+        public void afterJob(JobExecution jobExecution) {
+            int sum = jobExecution.getStepExecutions().stream()
+                    .mapToInt(StepExecution::getWriteCount)
+                    .sum();
+
+            log.info("afterJob : {}", sum);
+        }
+    }
+
+
+    // Annotation을 활용해 JobExecutionListener 생성
+    public static class SavePersonAnnotationJobExecutionListener {
+
+        @BeforeJob
+        public void beforeJob(JobExecution jobExecution) {
+            log.info("annotationBeforeJob");
+        }
+
+        @AfterJob
+        public void afterJob(JobExecution jobExecution) {
+            int sum = jobExecution.getStepExecutions().stream()
+                    .mapToInt(StepExecution::getWriteCount)
+                    .sum();
+
+            log.info("annotationAfterJob : {}", sum);
+        }
+    }
+
+
+    public static class SavePersonStepExecutionListener {
+
+        @BeforeStep
+        public void beforeStep(StepExecution stepExecution) {
+            log.info("beforeStep");
+        }
+
+        @AfterStep
+        public ExitStatus afterStep(StepExecution stepExecution) {
+            log.info("afterStep : {}", stepExecution.getWriteCount());
+
+            // 스프링배치는 Step의 상태를 stepExecution에 저장하기 때문에 해당 status를 리턴
+            return stepExecution.getExitStatus();
+        }
+    }
 }
