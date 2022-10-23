6815f26d041ba7ef41cc30bfd6a9c766e1fc9d45
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-24 07:19:45 +0900

    Update UserRoleUpdateDto.java

--- service/src/main/java/com/ot/service/dto/user/request/UserRoleUpdateDto.java
+++ service/src/main/java/com/ot/service/dto/user/request/UserRoleUpdateDto.java
@@ -1,4 +1,25 @@
 package com.ot.service.dto.user.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class UserRoleUpdateDto {
+
+    @Schema(description = "ID")
+    private Long id;
+
+    private List<String> userRoles;
+
+    @JsonIgnore
+    private String roles;
+
+    @Schema(description = "수정자", example = "ID")
+    @JsonIgnore
+    private Long updUserSeq;
 }
