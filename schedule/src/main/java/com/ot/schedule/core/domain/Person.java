a763a65e426dd17079b3f5e0de2be60ee6fd34da
developerOlive <67456294+developerOlive@users.noreply.github.com>
2022-10-24 07:21:18 +0900

    Update Person.java

--- schedule/src/main/java/com/ot/schedule/core/domain/Person.java
+++ schedule/src/main/java/com/ot/schedule/core/domain/Person.java
@@ -1,4 +1,51 @@
 package com.ot.schedule.core.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Slf4j
+@Table(name = "PERSON")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private String age;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    public Person(String name, String age, String address) {
+        this(0, name, age, address);
+    }
+
+    public Person(int id, String name, String age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean isNotEmptyName() {
+        return Objects.nonNull(this.name) && !name.isEmpty();
+    }
+
+    public Person unknownName() {
+        this.name = "UNKNOWN";
+        log.info("savePersonRetry : RecoveryCallback");
+        return this;
+    }
 }
